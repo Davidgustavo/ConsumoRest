8046d67c8af4c8edeab9eec8fa91a277892f37e1
David Alves <david.alves@cronapp.io>
2019-01-14 14:25:06 +0000

    Criação de uma nova função de consumo webservice.

--- src/main/java/blockly/Rest.java
+++ src/main/java/blockly/Rest.java
@@ -2,6 +2,7 @@ package blockly;
 
 import cronapi.*;
 import cronapi.rest.security.CronappSecurity;
+import java.util.Iterator;
 import java.util.concurrent.Callable;
 
 
@@ -28,6 +29,34 @@ public static Var Rest() throws Exception {
     mark = cronapi.json.Operations.getJsonOrMapField(mark, Var.valueOf("BRL"));
     mark = cronapi.json.Operations.getJsonOrMapField(mark, Var.valueOf("price"));
     cronapi.util.Operations.callClientFunction(Var.valueOf("cronapi.screen.changeValueOfField"), Var.valueOf("vars.rest"), mark);
+    return Var.VAR_NULL;
+   }
+ }.call();
+}
+
+/**
+ *
+ * @return Var
+ */
+// Descreva esta função...
+public static Var RestObjetos() throws Exception {
+ return new Callable<Var>() {
+
+   private Var api = Var.VAR_NULL;
+   private Var i = Var.VAR_NULL;
+   private Var objeto = Var.VAR_NULL;
+
+   public Var call() throws Exception {
+    api = cronapi.json.Operations.toList(cronapi.util.Operations.getURLFromOthers(Var.valueOf("GET"), Var.valueOf("application/json"), Var.valueOf("https://www.cronapp.io/wp-json/wp/v2/posts"), Var.VAR_NULL, Var.VAR_NULL));
+    for (Iterator it_i = api.iterator(); it_i.hasNext();) {
+        i = Var.valueOf(it_i.next());
+        objeto = cronapi.json.Operations.getJsonOrMapField(i, Var.valueOf("id"));
+        System.out.println(objeto.getObjectAsString());
+        objeto = cronapi.json.Operations.getJsonOrMapField(i, Var.valueOf("date"));
+        System.out.println(objeto.getObjectAsString());
+        objeto = cronapi.json.Operations.getJsonOrMapField(i, Var.valueOf("status"));
+        System.out.println(objeto.getObjectAsString());
+    } // end for
     return Var.VAR_NULL;
    }
  }.call();
